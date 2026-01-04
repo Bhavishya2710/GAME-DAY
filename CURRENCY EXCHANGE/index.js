42d2dc8079c61753d0ff5e17f7a964efe1d7342e
Bhavishya2710 <bhavishyashewaramani2710@gmail.com>
2026-01-04 15:13:20 +0530

    created CURRENCY EXCHANGE

---
+++ CURRENCY EXCHANGE/index.js
@@ -0,0 +1,6 @@
+// Real-time Currency Converter
+// Fetch real-time exchange rates from an API (e.g., https://api.exchangerate-api.com).
+// Allow users to convert amounts between two selected currencies.
+// Display the converted amount dynamically.
+
+let clist = fetch('https://v6.exchangerate-api.com/v6/500679e5c77f365b4fe63aaa/latest/USD')
