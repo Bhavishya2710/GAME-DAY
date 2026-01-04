2dd02f2d6e9a0ae1bf37e7dae9cd5abb8ca011b0
Bhavishya2710 <bhavishyashewaramani2710@gmail.com>
2026-01-04 17:40:11 +0530

    updated STUDENT RANKING DASHBOARD

--- STUDENT RANKING DASHBOARD/script.js
+++ STUDENT RANKING DASHBOARD/script.js
@@ -8,7 +8,7 @@ let students =[
 
 const table = document.getElementById("student-table");
 const searchInput = document.getElementById("search-input");
-const sortbtn = document.getElementById("sorting-btn");
+const sortBtn = document.getElementById("sorting-btn");
 const addBtn = document.getElementById("add-btn");
 const nameInput = document.getElementById("name-input");
 const scoreInput = document.getElementById("score-input");
@@ -22,4 +22,46 @@ function renderTable(data)
         table.innerHTML = `<tr><td colSpan="3" class="no-data">No Students Found</td></tr>`;
         return;
     }
+
+    data.forEach((student,index)=>{
+        const row = document.createElement("tr");
+        true.innerHTML = `<td>${index +1}</td>
+                          <td>${student.name}</td>
+                          <td>${student.score}</td>`;
+        table.appendChild(row);
+    })
 }
+
+renderTable(students);
+
+sortBtn.addEventListener("click",()=>{
+    students.sort((a,b)=> b.score - a.score);
+    applyFilter();
+})
+
+searchInput.addEventListener("input",applyFilter);
+
+function applyFilter()
+{
+    const query = searchInput.value.toLowerCase();
+    const filtered = students.filter(student => student.name.toLowerCase().includes(query));
+    renderTable(filtered);
+}
+
+addBtn.addEventListener("click",()=>{
+    const name = nameInput.value.trim();
+    const score = Number(scoreInput.value);
+
+    if(name === "" || isNaN(score))
+    {
+        alert("Enter valid name and score.");
+        return;
+    }
+
+    students.push({name ,score});
+    nameInput.value = "";
+    scoreInput.value = "";
+
+    students.sort((a,b) => b.score - a.score);
+    applyFilter();
+});
